56cf1eea595279dcb19cde15e3c374fe09a5a04c
Sevgi <sevgikertimm@gmail.com>
2021-02-28 10:38:43 -0500

    first commit

---
+++ Class1.java
@@ -0,0 +1,28 @@
+package package1;
+
+public class Class1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		int numberOfclass=23;
+		double num=34.5;
+		
+		//today we have seen on comment on Git and GitHub
+		
+		
+		int age =27;
+
+		boolean isGood = true;
+
+		char initial ='X';
+				String name ="Sevgi";
+				
+				System.out.println(age);
+				System.out.println(isGood);
+				System.out.println(initial + name);
+				
+		
+		
+	}
+
+}
